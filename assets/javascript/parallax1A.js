bdfdfa4e24a1db86e0fc0ecac0c204ed2302f314
Patrick Kearney <pckearney@yahoo.com>
2019-03-02 23:57:04 -0600

    abrupt and progressive effects description added

--- assets/javascript/parallax1A.js
+++ assets/javascript/parallax1A.js
@@ -1,15 +1,18 @@
 //SMOOTH SCROLLING DIRECTOR OBJECTS {id: "text" , start; "number", stop: "number", mark: }
 //start and stop refer to % of viewHeight
-const firstBoxDirector = {
-  scroll1: { id: "first-box", start: 75, stop: 25, mark: "top" }
+const scrollBoxDirector = {
+  scroll1: { id: "first-js-box", start: 75, stop: 25, mark: "top" },
+  scroll2: { id: "second-js-box", start: 75, stop: 25, mark: "top" },
+  scroll3: { id: "third-js-box", start: 80, stop: 55, mark: "top" },
+  scroll4: { id: "fourth-js-box", start: 80, stop: 55, mark: "top" }
 };
 
 const spanBoxDirector = {
-  scroll1: { id: "moving-frames-div", start: 45, stop: 40, mark: "top" },
-  scroll2: { id: "moving-frames-div", start: 42, stop: 37, mark: "top" },
-  scroll3: { id: "moving-frames-div", start: 39, stop: 34, mark: "top" },
-  scroll4: { id: "moving-frames-div", start: 36, stop: 31, mark: "top" },
-  position1: { id: "moving-frames-div", start: 30, stop: 0, mark: "top" }
+  scroll1: { id: "moving-frames-div", start: 55, stop: 50, mark: "top" },
+  scroll2: { id: "moving-frames-div", start: 52, stop: 47, mark: "top" },
+  scroll3: { id: "moving-frames-div", start: 49, stop: 44, mark: "top" },
+  scroll4: { id: "moving-frames-div", start: 46, stop: 41, mark: "top" },
+  position1: { id: "moving-frames-div", start: 40, stop: 20, mark: "top" }
 };
 
 const animationBoxDirector = {
@@ -20,44 +23,71 @@ const animationBoxDirector = {
 //SMOOTH SCROLLING ACTOR OBJECTS:
 //These contain both intial and final CSS values.
 //Position keys refer to percentages of relative parent.
-const firstBoxActor = {
-  id: "first-box",
-  color1: { red: 255, green: 255, blue: 0, opacity: 1 },
-  color2: { red: 71, green: 32, blue: 122, opacity: 1 },
-  background1: { red: 71, green: 32, blue: 122, opacity: 1 },
-  background2: { red: 255, green: 255, blue: 0, opacity: 1 }
+const scrollBoxActor = {
+  scroll1: {
+    id: "first-js-box",
+    color1: { red: 255, green: 255, blue: 0, opacity: 1 },
+    color2: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background1: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background2: { red: 255, green: 255, blue: 0, opacity: 1 }
+  },
+  scroll2: {
+    id: "second-js-box",
+    color1: { red: 255, green: 255, blue: 0, opacity: 1 },
+    color2: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background1: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background2: { red: 255, green: 255, blue: 0, opacity: 1 }
+  },
+  scroll3: {
+    id: "third-js-box",
+    color1: { red: 255, green: 255, blue: 0, opacity: 1 },
+    color2: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background1: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background2: { red: 255, green: 255, blue: 0, opacity: 1 }
+  },
+  scroll4: {
+    id: "fourth-js-box",
+    color1: { red: 255, green: 255, blue: 0, opacity: 1 },
+    color2: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background1: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background2: { red: 255, green: 255, blue: 0, opacity: 1 }
+  }
 };
 
 const spanBox1Actor = {
   id: "span-box-1",
   background1: { red: 0, green: 100, blue: 0, opacity: 1 },
   background2: { red: 71, green: 32, blue: 122, opacity: 1 },
-  position1: { top: 10, left: 5, bottom: "", right: "" },
-  position2: { top: 10, left: 25, bottom: "", right: "" }
+  position1: { top: 16, left: 16, bottom: "", right: "" },
+  //position2: { top: 0, left: 0, bottom: "", right: "" }
+  position2: { top: 67, left: 0, bottom: "", right: "" }
 };
 
 const spanBox2Actor = {
   id: "span-box-2",
   background1: { red: 0, green: 100, blue: 0, opacity: 1 },
   background2: { red: 255, green: 255, blue: 0, opacity: 1 },
-  position1: { top: 10, left: 25, bottom: "", right: "" },
-  position2: { top: 48, left: 25, bottom: "", right: "" }
+  position1: { top: 16, left: 50, bottom: "", right: "" },
+  //position2: { top: 0, left: 66.67, bottom: "", right: "" }
+  position2: { top: 0, left: 0, bottom: "", right: "" }
 };
 
 const spanBox3Actor = {
   id: "span-box-3",
   background1: { red: 0, green: 100, blue: 0, opacity: 1 },
   background2: { red: 255, green: 140, blue: 0, opacity: 1 },
-  position1: { top: 48, left: 25, bottom: "", right: "" },
-  position2: { top: 48, left: 5, bottom: "", right: "" }
+  position1: { top: 50, left: 50, bottom: "", right: "" },
+  //position2: { top: 67, left: 67, bottom: "", right: "" }
+  position2: { top: 0, left: 66.67, bottom: "", right: "" }
 };
 
 const spanBox4Actor = {
   id: "span-box-4",
   background1: { red: 0, green: 100, blue: 0, opacity: 1 },
   background2: { red: 255, green: 0, blue: 0, opacity: 1 },
-  position1: { top: 48, left: 5, bottom: "", right: "" },
-  position2: { top: 10, left: 5, bottom: "", right: "" }
+  position1: { top: 50, left: 16, bottom: "", right: "" },
+  //position2: { top: 67, left: 0, bottom: "", right: "" }
+  position2: { top: 67, left: 67, bottom: "", right: "" }
 };
 
 const circle1Actor = {
@@ -117,8 +147,14 @@ const circle4ActorB = {
 };
 
 window.onscroll = function() {
-  setColor(firstBoxActor, firstBoxDirector.scroll1);
-  setBackgroundColor(firstBoxActor, firstBoxDirector.scroll1);
+  setColor(scrollBoxActor.scroll1, scrollBoxDirector.scroll1);
+  setBackgroundColor(scrollBoxActor.scroll1, scrollBoxDirector.scroll1);
+  setColor(scrollBoxActor.scroll2, scrollBoxDirector.scroll2);
+  setBackgroundColor(scrollBoxActor.scroll2, scrollBoxDirector.scroll2);
+  setColor(scrollBoxActor.scroll3, scrollBoxDirector.scroll3);
+  setBackgroundColor(scrollBoxActor.scroll3, scrollBoxDirector.scroll3);
+  setColor(scrollBoxActor.scroll4, scrollBoxDirector.scroll4);
+  setBackgroundColor(scrollBoxActor.scroll4, scrollBoxDirector.scroll4);
   setBackgroundColor(spanBox1Actor, spanBoxDirector.scroll1);
   setBackgroundColor(spanBox2Actor, spanBoxDirector.scroll2);
   setBackgroundColor(spanBox3Actor, spanBoxDirector.scroll3);
