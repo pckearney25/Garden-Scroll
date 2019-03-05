2f76af0edd7fa521f96b5a7926648d2ea162f934
Patrick Kearney <pckearney@yahoo.com>
2019-03-05 14:54:55 -0600

    movie set in progress

--- assets/javascript/parallax1A.js
+++ assets/javascript/parallax1A.js
@@ -1,10 +1,14 @@
-//SMOOTH SCROLLING DIRECTOR OBJECTS {id: "text" , start; "number", stop: "number", mark: }
+//PROGRESSIVE DIRECTOR OBJECTS {id: "text" , start; "number", stop: "number", mark: }
 //start and stop refer to % of viewHeight
+//ABRUPT DIRECTOR OBJECTS {id: "text" , start; number, scrollUp: BOOLEAN, mark:"text" }
+
+//THE DIRECTORS!!!
 const scrollBoxDirector = {
   scroll1: { id: "first-js-box", start: 75, stop: 25, mark: "top" },
   scroll2: { id: "second-js-box", start: 75, stop: 25, mark: "top" },
   scroll3: { id: "third-js-box", start: 80, stop: 55, mark: "top" },
-  scroll4: { id: "fourth-js-box", start: 80, stop: 55, mark: "top" }
+  scroll4: { id: "fourth-js-box", start: 80, stop: 55, mark: "top" },
+  scroll5: { id: "fifth-js-box", start: 80, stop: 55, mark: "top" }
 };
 
 const spanBoxDirector = {
@@ -17,12 +21,15 @@ const spanBoxDirector = {
 
 const animationBoxDirector = {
   scroll1: { id: "animation-div", start: 70, stop: 55, mark: "top" },
-  animation1: { id: "animation-div", start: 52, scrollUp: true, mark: "top" }
+  animation1: { id: "animation-div", start: 40, scrollUp: true, mark: "top" }
 };
 
-//SMOOTH SCROLLING ACTOR OBJECTS:
+//PROGRASSIVE SCROLLING ACTOR OBJECTS:
 //These contain both intial and final CSS values.
 //Position keys refer to percentages of relative parent.
+//ANIMATION ACTOR OBJECTS {id: "text", upClass: "text", downClass: "text"}
+
+//THE ACTORS!!!
 const scrollBoxActor = {
   scroll1: {
     id: "first-js-box",
@@ -51,101 +58,79 @@ const scrollBoxActor = {
     color2: { red: 71, green: 32, blue: 122, opacity: 1 },
     background1: { red: 71, green: 32, blue: 122, opacity: 1 },
     background2: { red: 255, green: 255, blue: 0, opacity: 1 }
+  },
+  scroll5: {
+    id: "fifth-js-box",
+    color1: { red: 255, green: 255, blue: 0, opacity: 1 },
+    color2: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background1: { red: 71, green: 32, blue: 122, opacity: 1 },
+    background2: { red: 255, green: 255, blue: 0, opacity: 1 }
   }
 };
 
-const spanBox1Actor = {
-  id: "span-box-1",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 71, green: 32, blue: 122, opacity: 1 },
-  position1: { top: 16, left: 16, bottom: "", right: "" },
-  //position2: { top: 0, left: 0, bottom: "", right: "" }
-  position2: { top: 67, left: 0, bottom: "", right: "" }
+const spanBoxActor = {
+  span1: {
+    id: "span-box-1",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 71, green: 32, blue: 122, opacity: 1 },
+    position1: { top: 16, left: 16, bottom: "", right: "" },
+    position2: { top: 67, left: 0, bottom: "", right: "" }
+  },
+  span2: {
+    id: "span-box-2",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 255, green: 255, blue: 0, opacity: 1 },
+    position1: { top: 16, left: 50, bottom: "", right: "" },
+    position2: { top: 0, left: 0, bottom: "", right: "" }
+  },
+  span3: {
+    id: "span-box-3",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 255, green: 140, blue: 0, opacity: 1 },
+    position1: { top: 50, left: 50, bottom: "", right: "" },
+    position2: { top: 0, left: 66.67, bottom: "", right: "" }
+  },
+  span4: {
+    id: "span-box-4",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 255, green: 0, blue: 0, opacity: 1 },
+    position1: { top: 50, left: 16, bottom: "", right: "" },
+    position2: { top: 67, left: 67, bottom: "", right: "" }
+  }
 };
 
-const spanBox2Actor = {
-  id: "span-box-2",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 255, green: 255, blue: 0, opacity: 1 },
-  position1: { top: 16, left: 50, bottom: "", right: "" },
-  //position2: { top: 0, left: 66.67, bottom: "", right: "" }
-  position2: { top: 0, left: 0, bottom: "", right: "" }
+const circleActor = {
+  circle1: {
+    id: "circle-1",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 71, green: 32, blue: 122, opacity: 1 },
+    upClass: "bloom1",
+    downClass: "circle"
+  },
+  circle2: {
+    id: "circle-2",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 255, green: 255, blue: 0, opacity: 1 },
+    upClass: "bloom2",
+    downClass: "circle"
+  },
+  circle3: {
+    id: "circle-3",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 255, green: 140, blue: 0, opacity: 1 },
+    upClass: "bloom3",
+    downClass: "circle"
+  },
+  circle4: {
+    id: "circle-4",
+    background1: { red: 0, green: 100, blue: 0, opacity: 1 },
+    background2: { red: 255, green: 0, blue: 0, opacity: 1 },
+    upClass: "bloom4",
+    downClass: "circle"
+  }
 };
 
-const spanBox3Actor = {
-  id: "span-box-3",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 255, green: 140, blue: 0, opacity: 1 },
-  position1: { top: 50, left: 50, bottom: "", right: "" },
-  //position2: { top: 67, left: 67, bottom: "", right: "" }
-  position2: { top: 0, left: 66.67, bottom: "", right: "" }
-};
-
-const spanBox4Actor = {
-  id: "span-box-4",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 255, green: 0, blue: 0, opacity: 1 },
-  position1: { top: 50, left: 16, bottom: "", right: "" },
-  //position2: { top: 67, left: 0, bottom: "", right: "" }
-  position2: { top: 67, left: 67, bottom: "", right: "" }
-};
-
-const circle1Actor = {
-  id: "circle-1",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 71, green: 32, blue: 122, opacity: 1 }
-};
-
-const circle2Actor = {
-  id: "circle-2",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 255, green: 255, blue: 0, opacity: 1 }
-};
-
-const circle3Actor = {
-  id: "circle-3",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 255, green: 140, blue: 0, opacity: 1 }
-};
-
-const circle4Actor = {
-  id: "circle-4",
-  background1: { red: 0, green: 100, blue: 0, opacity: 1 },
-  background2: { red: 255, green: 0, blue: 0, opacity: 1 }
-};
-
-//ANIMATION DIRECTOR OBJECTS {id: "text" , start; number, scrollUp: BOOLEAN, mark:"text" }
-const circleAnimationDirector2 = {
-  id: "animation-div",
-  start: 35,
-  scrollUp: true,
-  mark: "top"
-};
-//ANIMATION ACTOR OBJECTS {id: "text", upClass: "text", downClass: "text"}
-const circle1ActorB = {
-  id: "circle-1",
-  upClass: "bloom1",
-  downClass: "circle"
-};
-
-const circle2ActorB = {
-  id: "circle-2",
-  upClass: "bloom2",
-  downClass: "circle"
-};
-
-const circle3ActorB = {
-  id: "circle-3",
-  upClass: "bloom3",
-  downClass: "circle"
-};
-
-const circle4ActorB = {
-  id: "circle-4",
-  upClass: "bloom4",
-  downClass: "circle"
-};
-
+//THE SCRIPT FOR THE DIRECTORS AND ACTORS!!!
 window.onscroll = function() {
   setColor(scrollBoxActor.scroll1, scrollBoxDirector.scroll1);
   setBackgroundColor(scrollBoxActor.scroll1, scrollBoxDirector.scroll1);
@@ -155,20 +140,22 @@ window.onscroll = function() {
   setBackgroundColor(scrollBoxActor.scroll3, scrollBoxDirector.scroll3);
   setColor(scrollBoxActor.scroll4, scrollBoxDirector.scroll4);
   setBackgroundColor(scrollBoxActor.scroll4, scrollBoxDirector.scroll4);
-  setBackgroundColor(spanBox1Actor, spanBoxDirector.scroll1);
-  setBackgroundColor(spanBox2Actor, spanBoxDirector.scroll2);
-  setBackgroundColor(spanBox3Actor, spanBoxDirector.scroll3);
-  setBackgroundColor(spanBox4Actor, spanBoxDirector.scroll4);
-  moveAbsoluteObject(spanBox1Actor, spanBoxDirector.position1);
-  moveAbsoluteObject(spanBox2Actor, spanBoxDirector.position1);
-  moveAbsoluteObject(spanBox3Actor, spanBoxDirector.position1);
-  moveAbsoluteObject(spanBox4Actor, spanBoxDirector.position1);
-  setBackgroundColor(circle1Actor, animationBoxDirector.scroll1);
-  setBackgroundColor(circle2Actor, animationBoxDirector.scroll1);
-  setBackgroundColor(circle3Actor, animationBoxDirector.scroll1);
-  setBackgroundColor(circle4Actor, animationBoxDirector.scroll1);
-  animateObject(circle1ActorB, animationBoxDirector.animation1);
-  animateObject(circle2ActorB, animationBoxDirector.animation1);
-  animateObject(circle3ActorB, animationBoxDirector.animation1);
-  animateObject(circle4ActorB, animationBoxDirector.animation1);
+  setColor(scrollBoxActor.scroll5, scrollBoxDirector.scroll5);
+  setBackgroundColor(scrollBoxActor.scroll5, scrollBoxDirector.scroll5);
+  setBackgroundColor(spanBoxActor.span1, spanBoxDirector.scroll1);
+  setBackgroundColor(spanBoxActor.span2, spanBoxDirector.scroll2);
+  setBackgroundColor(spanBoxActor.span3, spanBoxDirector.scroll3);
+  setBackgroundColor(spanBoxActor.span4, spanBoxDirector.scroll4);
+  moveAbsoluteObject(spanBoxActor.span1, spanBoxDirector.position1);
+  moveAbsoluteObject(spanBoxActor.span2, spanBoxDirector.position1);
+  moveAbsoluteObject(spanBoxActor.span3, spanBoxDirector.position1);
+  moveAbsoluteObject(spanBoxActor.span4, spanBoxDirector.position1);
+  setBackgroundColor(circleActor.circle1, animationBoxDirector.scroll1);
+  setBackgroundColor(circleActor.circle2, animationBoxDirector.scroll1);
+  setBackgroundColor(circleActor.circle3, animationBoxDirector.scroll1);
+  setBackgroundColor(circleActor.circle4, animationBoxDirector.scroll1);
+  animateObject(circleActor.circle1, animationBoxDirector.animation1);
+  animateObject(circleActor.circle2, animationBoxDirector.animation1);
+  animateObject(circleActor.circle3, animationBoxDirector.animation1);
+  animateObject(circleActor.circle4, animationBoxDirector.animation1);
 };
